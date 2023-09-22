95f50d2004b061a11853a6e46a9a657722a82f94
TAREKMOHAMED53 <tarektito341@gmail.com>
2023-09-22 22:01:47 +0300

    task5

---
+++ task5/inc/header.php
@@ -0,0 +1,13 @@
+<?php session_start(); ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>Php Project</title>
+
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/all.min.css">
+
+</head>
